5aadebb5e51dcae6c4e5b7e3505d7694ce0ff2d0
Andrew Hart <andrewthart098@gmail.com>
2015-11-17 17:12:53 -0500

    running homepage

--- app/app.js
+++ app/app.js
@@ -5,8 +5,12 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.homepage',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+
+  $routeProvider
+  	.otherwise({redirectTo: '/homepage'});;
 }]);
